f4fb70ab7b53f72ac01d93ba38447689f422d502
Atallah <atallahhezbor@gmail.com>
2016-04-12 12:01:01 -0400

    Adds method for computing tour distance

--- main.c
+++ main.c
@@ -6,10 +6,29 @@
 #include <stdio.h>
 #include <mpi.h>
 
+
+
+
+
+int calculate_tour_distance(int * tour, int tour_size, int ** distances);
+
 int main(int argc, char **argv) {
     MPI_Init(&argc, &argv); // Initialize MPI
 
     printf("Hello World\n");
 
     MPI_Finalize(); // Close MPI
+}
+
+int calculate_tour_distance(int * tour, int tour_size, int ** distances) {	
+	int i;
+	int distance = 0;
+	// Calculate distance to end of tour
+	for (i=0; i < tour_size-1; i++) 
+	{
+		distance += distances[tour[i]][tour[i+1]];
+	}
+	// Add distance back to start
+	distance += distances[tour[tour_size]][tour[0]];
+	return distance;
 }
